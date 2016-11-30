841c58241118caadb7abebd959d3e15153a0df9e
Siddharth Kumar <sidkumar@live.com>
2016-11-30 16:23:58 -0600

    Initial commit

---
+++ app/src/main/java/com/siddharthkumar/android/helpfulweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.siddharthkumar.android.helpfulweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
