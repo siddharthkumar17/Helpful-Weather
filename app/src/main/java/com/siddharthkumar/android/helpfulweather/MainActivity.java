f3dfbb6d211eb3944e01ca9e122e7c53d84ca698
Siddharth Kumar <sidkumar@live.com>
2016-12-03 16:44:03 -0600

    Weather support

--- app/src/main/java/com/siddharthkumar/android/helpfulweather/MainActivity.java
+++ app/src/main/java/com/siddharthkumar/android/helpfulweather/MainActivity.java
@@ -6,29 +6,43 @@ import android.location.Location;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+import org.json.*;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.location.LocationServices;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements OnConnectionFailedListener, ConnectionCallbacks{
     TextView temp;
     GoogleApiClient mGoogleApiClient;
     Location lastLocation;
     final String API_KEY = "d9a03c069a7bf250a30a3229e82a0a9b";
+    final int MY_PERMISSIONS_REQUEST_LOCATION = 17;
+    final int MY_PERMISSIONS_REQUEST_INTERNET = 18;
+    boolean permissionInternet = true;
+    boolean permissionLocation = true;
+    final String TAG = "MAIN";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         temp = (TextView)findViewById(R.id.temp);
 
@@ -45,6 +59,37 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
 
 
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    permissionLocation = true;
+
+
+
+
+                        getWeather();
+
+
+                } else {
+
+                    permissionLocation = false;
+
+
+                }
+                return;
+            }
+
+
+
+        }
     }
     protected void onStart() {
         mGoogleApiClient.connect();
@@ -63,18 +108,57 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)== PackageManager.PERMISSION_GRANTED)
         {
-            lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if(lastLocation!=null)
-                temp.setText(lastLocation.getLatitude()+", "+lastLocation.getLongitude());
+            getWeather();
 
         }
         else{
-            Toast.makeText(this,"Please give location access to find accurate weather",Toast.LENGTH_LONG).show();
-            lastLocation=null;
+
+
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && permissionLocation) {
+
+
+
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+
+            }
+
         }
 
+
+    }
+
+    public void getWeather(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if(lastLocation!=null){
+                try{
+                    String jsonWeatherData = new FetchWeatherTask().execute(lastLocation.getLatitude()+" "+lastLocation.getLongitude()).get();
+                    if(jsonWeatherData==null)
+                        Log.e(TAG,"Error json");
+                    else{
+                       // temp.setText(jsonWeatherData);
+                        JSONObject jsonObject = new JSONObject(jsonWeatherData);
+                        temp.setText("The weather in "+jsonObject.getString("name")+" is "+jsonObject.getJSONObject("weather").getString("main"));
+
+                    }
+
+                }
+                catch (Exception e){
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
 
     }
 
@@ -94,14 +178,21 @@ public class MainActivity extends AppCompatActivity implements OnConnectionFaile
             for(String s: strings){
 
                 try{
-                    String url = "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid="+API_KEY;
-                   // InputStream input = new URL.openStream();
 
+                    Scanner sc = new Scanner(s);
+                    String latitude=sc.next();
+                    String longitude=sc.next();
+                    String url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+API_KEY;
+                    InputStream input = new URL(url).openStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    String line = reader.readLine();
+                    return line;
                 }
                 catch(Exception e){
-                    Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    Log.e(TAG, e.getMessage());
 
                 }
+
 
 
 
